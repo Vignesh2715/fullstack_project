dfa0409715b7f00525f4ecdf269cb9db5c0373a8
Vignesh2715 <vigneswarareddymadduri@gmail.com>
2026-05-15 22:41:17 +0530

    added login funtionality

--- frontend/src/pages/Cart/Cart.jsx
+++ frontend/src/pages/Cart/Cart.jsx
@@ -1,7 +1,23 @@
-import React from "react";
+import React, { useContext } from "react";
 import "./Cart.css";
+import { StoreContext } from "../../context/StoreContext";
 const Cart = () => {
-  return <div></div>;
+  const { cartItems, food_list, removeFromCart } = useContext(StoreContext);
+
+  return (
+    <div className="cart">
+      <div className="cart-items">
+        <div className="cart-items-title">
+          <p>Items</p>
+          <p>Title</p>
+          <p>Price</p>
+          <p> Quantity</p>
+          <p>Total</p>
+          <p>Remove</p>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Cart;
