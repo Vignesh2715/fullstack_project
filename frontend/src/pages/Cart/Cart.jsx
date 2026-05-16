4bd2cc449f68e02531816a7ca754087aa886f75d
Vignesh2715 <vigneswarareddymadduri@gmail.com>
2026-05-16 22:47:47 +0530

    created cart page and updated css for cart page

--- frontend/src/pages/Cart/Cart.jsx
+++ frontend/src/pages/Cart/Cart.jsx
@@ -15,6 +15,27 @@ const Cart = () => {
           <p>Total</p>
           <p>Remove</p>
         </div>
+        <br />
+        <hr />
+        {food_list.map((item, index) => {
+          if (cartItems[item._id] > 0) {
+            return (
+              <div>
+                <div className="cart-items-title cart-items-item">
+                  <img src={item.image} alt="" />
+                  <p>{item.name}</p>
+                  <p>${item.price}</p>
+                  <p>{cartItems[item._id]}</p>
+                  <p>${item.price * cartItems[item._id]}</p>
+                  <p onClick={() => removeFromCart(item._id)} className="cross">
+                    x
+                  </p>
+                </div>
+                <hr />
+              </div>
+            );
+          }
+        })}
       </div>
     </div>
   );
