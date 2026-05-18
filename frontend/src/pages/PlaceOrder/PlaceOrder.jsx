4771eefb9132390f630c4862491c643db7a5b292
Vignesh2715 <vigneswarareddymadduri@gmail.com>
2026-05-18 17:36:55 +0530

    updated cart page and made responsive

--- frontend/src/pages/PlaceOrder/PlaceOrder.jsx
+++ frontend/src/pages/PlaceOrder/PlaceOrder.jsx
@@ -1,7 +1,16 @@
 import React from "react";
 import "./PlaceOrder.css";
 const PlaceOrder = () => {
-  return <div></div>;
+  return (
+    <form className="place-order">
+      <div className="place-order">
+        <div className="place-order-left">
+          <p className="title">Delivery Information</p>
+          <div className="multi-fields"></div>
+        </div>
+      </div>
+    </form>
+  );
 };
 
 export default PlaceOrder;
