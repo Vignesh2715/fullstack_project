add73de0666f68b1e4f424270402577f1441b827
Vignesh2715 <vigneswarareddymadduri@gmail.com>
2026-05-24 17:42:31 +0530

    feat: add form page, routes, and page styling

--- admin/src/pages/Add/Add.jsx
+++ admin/src/pages/Add/Add.jsx
@@ -1,7 +1,55 @@
 import React from "react";
 import "./Add.css";
+import { assets } from "../../assets/assets";
 const Add = () => {
-  return <div></div>;
+  return (
+    <div className="add">
+      <form className="flex-col">
+        <div className="add-img-upload flex-col">
+          <p>Upload Image</p>
+          <label htmlFor="image">
+            <img src={assets.upload_area} alt="" />
+          </label>
+          <input type="file" id="image" hidden required />
+        </div>
+        <div className="add-product-name flex-col">
+          <p>Product name</p>
+          <input type="text" name="name" placeholder="Type here" />
+        </div>
+        <div className="add-product-description flex-col">
+          <p>Product description</p>
+          <textarea
+            name="description"
+            rows="6"
+            placeholder="Write content here"
+            required
+          ></textarea>
+        </div>
+        <div className="add-category-price">
+          <div className="add-category flex-col">
+            <p>Product category</p>
+            <select name="category">
+              <option value="Salad">Salad</option>
+              <option value="Rolls">Rolls</option>
+              <option value="Desert">Desert</option>
+              <option value="Sandwich">Sandwich</option>
+              <option value="Cake">Cake</option>
+              <option value=" Pure veg">Pure veg</option>
+              <option value="pasta">Pasta</option>
+              <option value="Noodles">Noodles</option>
+            </select>
+          </div>
+          <div className="add-price flex-col">
+            <p>Product price</p>
+            <input type="Number" name="price" placeholder="$20" />
+          </div>
+        </div>
+        <button type="Submit" className="add-btn">
+          ADD
+        </button>
+      </form>
+    </div>
+  );
 };
 
 export default Add;
