c7bf9768718ad8f68748ce09dc78ad7ab908f0a7
Vignesh2715 <vigneswarareddymadduri@gmail.com>
2026-06-18 19:34:09 +0530

    added payment integration using stripe

--- backend/controllers/orderController.js
+++ backend/controllers/orderController.js
@@ -9,6 +9,8 @@ const stripe = new Stripe(process.env.STRIPE_SECRET_KEY);
 // placing usermodel from frontend
 
 const placeOrder = async (req, res) => {
+  const frontend_url = "http://localhost:5173";
+
   try {
     const newOrder = new orderModel({
       userId: req.body.userId,
@@ -17,8 +19,45 @@ const placeOrder = async (req, res) => {
       address: req.body.address,
     });
     await newOrder.save();
+
+    // to clear user cart
     await userModel.findByIdAndUpdate(req.body.userId, { cartData: {} });
-  } catch (error) {}
+
+    // to create payment link
+    const line_items = req.body.items.map((item) => ({
+      price_data: {
+        currency: "inr",
+        product_data: {
+          name: item.name,
+        },
+        unit_amount: item.price * 100 * 90,
+      },
+      quantity: item.quantity,
+    }));
+
+    line_items.push({
+      price_data: {
+        currency: "inr",
+        product_data: {
+          name: "Delivery Charges",
+        },
+        unit_amount: 2 * 100 * 90,
+      },
+      quantity: 1,
+    });
+
+    const session = await stripe.checkout.sessions.create({
+      line_items: line_items,
+      mode: "payment",
+      success_url: `${frontend_url}/verify?success=true&orderId=${newOrder._id}`,
+      cancel_url: `${frontend_url}/verify?success=false&orderId=${newOrder._id}`,
+    });
+
+    res.json({ success: true, session_url: session.url });
+  } catch (error) {
+    console.log(error);
+    res.json({ success: false, message: "Error" });
+  }
 };
 
 export { placeOrder };
